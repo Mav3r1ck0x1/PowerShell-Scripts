bd3915877843f0a203c782fbc55ebc1acb490dfd
Blake Blackwell <55414177+Maverick-cmd@users.noreply.github.com>
2022-04-20 12:25:56 -0700

    Initial Commit
    4/20/2022  "422 is 420 too"

---
+++ WindowsUpdateWithGUI.ps1
@@ -0,0 +1,29 @@
+﻿$winVer = [System.Environment]::OSVersion.Version.Major
+$dir = 'C:\_Windows_FU\packages'
+
+mkdir $dir
+
+if ($winVer -eq 10)
+    {  
+        $webClient = New-Object System.Net.WebClient
+        $url = 'https://go.microsoft.com/fwlink/?LinkID=799445'
+        $file = "$($dir)\Win10Upgrade.exe"
+        $webClient.DownloadFile($url,$file)
+        Start-Process -FilePath $file -ArgumentList '/auto upgrade /copylogs $dir'
+        Add-Type -AssemblyName System.Windows.Forms
+        sleep 4
+        [System.Windows.Forms.SendKeys]::SendWait("{TAB}{TAB}{ENTER}")
+        sleep 4
+        [System.Windows.Forms.SendKeys]::SendWait("{TAB}{ENTER}")
+        
+        } 
+    
+    else 
+
+        {
+            echo "This is Not Windows10 OS "
+        }
+    
+sleep 10
+
+Remove-Item "C:\_Windows_FU" -Recurse -Force -Confirm:$false
