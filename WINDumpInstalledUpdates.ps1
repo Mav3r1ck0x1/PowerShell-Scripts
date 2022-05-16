9500252fe02331a3b09701b58f6236c2b6c06cee
Blake Blackwell <55414177+Maverick-cmd@users.noreply.github.com>
2022-05-16 10:09:45 -0700

    Commit WINDumpInstalledUpdates

---
+++ WINDumpInstalledUpdates.ps1
@@ -0,0 +1,9 @@
+﻿#Get Host and print 
+$hostname = Get-Content env:computername
+Write-Output "Hostname: $hostname " 
+
+$Session = New-Object -ComObject Microsoft.Update.Session
+$Searcher = $Session.CreateUpdateSearcher()
+$HistoryCount = $Searcher.GetTotalHistoryCount()
+$Updates = $Searcher.QueryHistory(0,$HistoryCount)
+$Updates |  Select Title,@{l='Name';e={$($_.Categories).Name}},Date
